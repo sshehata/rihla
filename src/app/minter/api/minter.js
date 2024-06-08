e18fda710582cf26d7a11dbc46058014a22f40d3
Ivan Butkevych <ivan@provablelabs.com>
2024-06-08 21:27:18 +0200

    --amend

--- src/app/minter/api/minter.js
+++ src/app/minter/api/minter.js
@@ -11,8 +11,8 @@ export async function mint() {
     const nftTransaction = {
         TransactionType: "NFTokenMint",
         Account:  wallet.classicAddress,
-        URI: xrpl.convertStringToHex(`Rihla-Test-URI`),
-        Flags: 8,
+        URI: xrpl.convertStringToHex(`Rihla-Test-URI-1`),
+        Flags: 0,
         NFTokenTaxon: 0 //Required, but if you have no use for it, set to zero.
     }
 
@@ -21,7 +21,7 @@ export async function mint() {
     const result = await client.submitAndWait(signedTx.tx_blob);
     console.log('NFT Mint Result:', result);
 
-    // const tx = await client.submitAndWait(nftTransaction, { wallet: wallet} )
+
     // // const nfts = await client.request({
     // //     method: "account_nfts",
     // //     account: wallet.classicAddress
@@ -30,3 +30,22 @@ export async function mint() {
 
     await client.disconnect();
 }
+
+export async function get_nfts() {
+    const wallet = xrpl.Wallet.fromSeed(config.rihlaWallet.secret)
+
+    const client = new xrpl.Client(config.xrplConfig.network); // Use testnet for development
+    await client.connect();
+
+    const nfts = await client.request({
+         method: "account_nfts",
+         account: wallet.classicAddress
+    })
+    console.log('NFTs:', JSON.stringify(nfts,null,2));
+
+    await client.disconnect();
+}
+
+
+// token-1: '00080000BCE0D6D1A517A8209ABD7602AC331866E647132D1BBA65400014A6A5'
+// token-2: '00080000BCE0D6D1A517A8209ABD7602AC331866E647132D32A036410014A6A6'
--- src/app/minter/api/route.js
+++ src/app/minter/api/route.js
@@ -1,7 +1,8 @@
 import * as minter from "@/app/minter/api/minter";
 
 export async function GET(request) {
-  await minter.mint();
+  //await minter.mint();
+  await minter.get_nfts()
   return Response.json({"message": "hello"})
 }
 

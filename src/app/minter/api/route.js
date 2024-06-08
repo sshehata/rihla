23c28ff3aadc8d9f7a109ec0345416e63503e0d6
Ivan Butkevych <ivan@provablelabs.com>
2024-06-08 22:08:09 +0200

    --amend

--- src/app/minter/api/route.js
+++ src/app/minter/api/route.js
@@ -1,8 +1,20 @@
 import * as minter from "@/app/minter/api/minter";
+import crypto from "crypto";
+import {get_nfts_for_rihlia} from "@/app/minter/api/minter";
 
 export async function GET(request) {
-  //await minter.mint();
-  await minter.get_nfts()
+  // const artUniqueData = { "author": "you" };
+  // const artUniqueDataStr = JSON.stringify(artUniqueData);
+  // const artUniqueReference = crypto.createHash('sha256').update(artUniqueDataStr)
+  //     .digest('hex').toUpperCase()
+  //
+  // const nftId = await minter.mint(artUniqueReference);
+  // console.log('NFT CREATED WITH ID: ', nftId)
+
+  //await minter.get_nfts()
+
+  await get_nfts_for_rihlia()
+
   return Response.json({"message": "hello"})
 }
 

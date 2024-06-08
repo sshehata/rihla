404bffe92723fa6c969c8e6133d72adf48f83552
Ivan Butkevych <ivan@provablelabs.com>
2024-06-08 23:14:23 +0200

    --amend

--- src/app/minter/api/route.js
+++ src/app/minter/api/route.js
@@ -1,5 +1,8 @@
 import * as minter from "@/app/minter/api/minter";
 
+// test operational wallet addr -- r4EadzUh7EDbBdHssAKjPk3cmyJ4YyMzZ3
+// test operational wallet seed -- sEd7r8x2pJFPpQY7YNermVqu4nqePYv
+
 export async function GET(request) {
   // const artUniqueReference = "art-unique-reference-" + Math.floor(Math.random() * 1000);
   // const nftId = await minter.mint(artUniqueReference);
@@ -7,7 +10,8 @@ export async function GET(request) {
 
   //await minter.get_nfts_for_rihlia()
 
-  await minter.get_rihlia_balance()
+  //await minter.get_rihlia_balance()
+  await minter.get_balance_for_rhilia()
 
   return Response.json({"message": "hello"})
 }

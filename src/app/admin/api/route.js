7abf92b87860eefa8b449be3beb72bf3b4463566
samy <samy@provablelabs.com>
2024-06-08 21:38:18 +0200

    add upload

--- src/app/admin/api/route.js
+++ src/app/admin/api/route.js
@@ -4,13 +4,15 @@ import multer from "multer";
 // Configure multer storage
 const upload = multer({
   storage: multer.memoryStorage(), // Store files in memory
+    destination: './public',
   limits: { fileSize: 5 * 1024 * 1024 }, // Set a file size limit (5MB here)
 });
 
 
 export async function POST(request) {
     const data = await request.formData()
-    console.log(data);
+    console.log(data)
+    console.log(data.get('artwork').name)
     addListing({...data, id: getListings().length + 1})
 
     return Response.json({})
